eece6b2fd45f48ba1088fee7e4425fc59db24b7e
Upik Saleh <upxsal@gmail.com>
2019-08-24 13:50:54 +0800

    fix class_exist in dev

--- config/console.php
+++ config/console.php
@@ -34,7 +34,7 @@ $config = [
     */
 ];
 
-if (YII_ENV_DEV) {
+if (YII_ENV_DEV && class_exists('\yii\gii\Module')) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
